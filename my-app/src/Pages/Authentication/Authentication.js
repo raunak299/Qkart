8c214928fe6023226372bf670d317ee3980763f9
raunak <iamraunak299@gmail.com>
2022-09-27 00:05:38 +0530

    publish

--- my-app/src/Pages/Authentication/Authentication.js
+++ my-app/src/Pages/Authentication/Authentication.js
@@ -223,12 +223,26 @@ function Authentication() {
         // )
 
 
-        const response = await axios.post('/api/auth/login', {
-            email: "johndoe@gmail.com",
-            password: "johnDoe123",
-        })
-        console.log(response.data.encodedToken);
-        applyData(response);
+        // const response = await axios.post('/api/auth/login', {
+        //     email: "johndoe@gmail.com",
+        //     password: "johnDoe123",
+        // })
+        // console.log(response.data.encodedToken);
+        // applyData(response);
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'a6eb7e32f3msh57a4a6f42d0fa31p1d943bjsn6713c4073c3f',
+                'X-RapidAPI-Host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com'
+            }
+        };
+
+        fetch('https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=us&lang=en&currentpage=0&pagesize=30&categories=men_all&concepts=H%26M%20MAN', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+
 
 
     }
